ce152c605e26010620913314357690fe35347737
Gu Chao <guchao2000@gmail.com>
2024-02-14 06:40:09 +0000

    changes 20240214-1

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,14 @@
-import json
- 
+import json, os
+from download import download_file
+from upload import upload_s3
+import environment_variables
+
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda using gha downloader!')
-    }
+    file = '2015-01-01-15.json.gz'
+    download_res = download_file(file)
+    bucket = environment_variables.BUCKET_NAME
+    upload_res = upload_s3 (
+        download_res.content, 
+        bucket, 
+        file)
+    return upload_res
